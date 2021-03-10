9b7a382a88acf4636d1076a8df95ea5aa6890a00
Chris <ChristianFigueroaMontano@outlook.com>
2021-03-09 22:04:45 -0600

    Change sendfile directory+

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.static('../library'))
 app.set('view engine', 'pug')
 
 app.get('/',function(req,res){
-    res.sendFile('index.html',{root:__dirname})
+    res.sendFile('./index.html',{root:__dirname})
 })
 
 app.listen(PORT, ()=>console.log (`Example app listening on port `))
