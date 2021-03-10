3da6dd59134cdd0bd9f0274b354e0b792d24d02b
Chris <ChristianFigueroaMontano@outlook.com>
2021-03-09 20:50:31 -0600

    Changes app listen

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require ('express')
 const app = express()
-const port = 3000 || process.env.PORT;
+ 
 
 app.use(express.static('./library'))
 
@@ -10,4 +10,4 @@ app.get('/',function(req,res){
     res.sendFile('index.html',{root:__dirname})
 })
 
-app.listen(port, ()=>console.log (`Example app listening on port${port}`))
+app.listen(3000 || process.env.PORT, ()=>console.log (`Example app listening on port${port}`))
